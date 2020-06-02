04986ee85f71f438589954bf9b25a00d365cc344
manan <manangoel012@gmail.com>
2020-06-02 21:39:11 +0530

    bug-fix

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
     next();
 })
 
-// app.use(function(error, req, res, next) {
+// app.use((error, req, res, next) => {
 //     console.log(error);
 //     const status = error.statusCode || 500;
 //     const message = error.message;
@@ -32,12 +32,11 @@ app.use((req, res, next) => {
 app.use('/api', apiroutes);
 
 mongoose.connect(
-        'mongodb+srv://admin-hrms:' + 'BUTTONS007' + '@cluster0-kldnh.mongodb.net/HRMS?retryWrites=true&w=majority', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        }
-    )
-    .then(console.log('hello'))
-    .catch(err => console.log(err))
+    'mongodb+srv://admin-hrms:' + 'BUTTONS007' + '@cluster0-kldnh.mongodb.net/HRMS?retryWrites=true&w=majority',{
+         useNewUrlParser: true, useUnifiedTopology: true 
+    }
+)
+.then(console.log('hello'))
+.catch(err => console.log(err))
 
 app.listen(process.env.PORT || 3000);
