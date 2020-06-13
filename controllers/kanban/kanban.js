92cd6140d5c6569472f32cfd68ad36b4893ef0ca
Manan <manangoel012@gmail.com>
2020-06-13 19:03:26 +0530

    kanban updated

--- controllers/kanban/kanban.js
+++ controllers/kanban/kanban.js
@@ -43,81 +43,76 @@ exports.getAllTasks = (req, res, next) => {
 exports.saveTask = (req, res, next) => {
     const tasks = req.body.tasks
     const _user = req.body._user
-    const adminId = req.userId;
+    const userId = req.userId;
     //Delete from database if task not in tasks
     //Also delete any attachments not present
-    User.findById(adminId)
+    User.findById(userId)
         .then(success => {
             if (success == null || success.length < 1) {
                 return res.status(404).json({
                     success: 'false',
-                    message: 'Admin not found'
+                    message: 'user not found'
                 })
             } else {
-                if (success.role == 'admin') {
-                    Task.find({ _user: _user }, (err, docs) => {
-                        docs.forEach(doc => {
-                            var found = false
-                            for (var key in tasks) {
-                                var task = tasks[key]
-                                if (doc.id === task.id) {
-                                    found = true
-                                    doc.attachments.forEach(attachment => {
-                                        console.log("In delete")
-                                        if (!task["attachments"].includes(attachment)) {
-                                            try {
-                                                fs.unlinkSync('./uploads/' + attachment)
-                                                console.log("File Deleted")
-                                            } catch (err) {
-                                                console.log(err)
-                                            }
+
+                Task.find({ _user: _user }, (err, docs) => {
+                    docs.forEach(doc => {
+                        var found = false
+                        for (var key in tasks) {
+                            var task = tasks[key]
+                            if (doc.id === task.id) {
+                                found = true
+                                doc.attachments.forEach(attachment => {
+                                    console.log("In delete")
+                                    if (!task["attachments"].includes(attachment)) {
+                                        try {
+                                            fs.unlinkSync('./uploads/' + attachment)
+                                            console.log("File Deleted")
+                                        } catch (err) {
+                                            console.log(err)
                                         }
-                                    })
-                                    break
-                                }
-                            }
-                            if (!found) {
-                                Task.deleteOne({ _user: _user, id: doc.id }, (err, docs) => {
-                                    if (!err) {
-                                        console.log("Deleted task")
-                                    } else {
-                                        res.json(err)
                                     }
                                 })
+                                break
                             }
-                        })
+                        }
+                        if (!found) {
+                            Task.deleteOne({ _user: _user, id: doc.id }, (err, docs) => {
+                                if (!err) {
+                                    console.log("Deleted task")
+                                } else {
+                                    res.json(err)
+                                }
+                            })
+                        }
                     })
+                })
 
-                    //Insert into database if task in tasks or update if required
-                    for (var key in tasks) {
-                        var task = tasks[key]
-                        Task.findOneAndUpdate({ _user, _user, id: task.id }, task, { upsert: true }, (err, docs) => {
-                            if (!err) {
-                                if (docs) {
-                                    docs.toObject()
-                                    for (var key in docs) {
-                                        if (task[key] && docs[key] !== task[key] || key === "checklist") {
-                                            Task.update({ id: task["id"] }, { $set: { key: task[key] } }, (err, docs) => {
-                                                if (err) {
-                                                    res.json(err)
-                                                }
-                                            })
-                                        }
+                //Insert into database if task in tasks or update if required
+                for (var key in tasks) {
+                    var task = tasks[key]
+                    Task.findOneAndUpdate({ _user, _user, id: task.id }, task, { upsert: true }, (err, docs) => {
+                        if (!err) {
+                            if (docs) {
+                                docs.toObject()
+                                for (var key in docs) {
+                                    if (task[key] && docs[key] !== task[key] || key === "checklist") {
+                                        Task.update({ id: task["id"] }, { $set: { key: task[key] } }, (err, docs) => {
+                                            if (err) {
+                                                res.json(err)
+                                            }
+                                        })
                                     }
                                 }
-                            } else {
-                                res.json(err)
                             }
-                        })
-                    }
-                    res.json({ "message": "saved" })
-
-                } else {
-                    return res.status(401).json({
-                        success: 'false',
-                        message: 'Unauthorized access'
+                        } else {
+                            res.json(err)
+                        }
                     })
                 }
+                res.json({ "message": "saved" })
+
+
             }
 
         })
@@ -163,60 +158,55 @@ exports.getAllBuckets = (req, res, next) => {
 exports.saveBucket = (req, res, next) => {
     const buckets = req.body.buckets
     const _user = req.body._user
-    const adminId = req.userId;
+    const userId = req.userId;
     //Delete bucket if database contains a bucket not in buckets
-    User.findById(adminId)
+    User.findById(userId)
         .then(success => {
             if (success == null || success.length < 1) {
-                return res.status(500).json({
+                return res.status(404).json({
                     success: 'false',
-                    message: 'Some error occurred'
+                    message: 'User not found'
                 })
             } else {
-                if (success.role == 'admin') {
-                    Bucket.find({ _user: _user }, (err, docs) => {
-                        docs.forEach(doc => {
-                            var found = false
-                            for (var key in buckets) {
-                                var bucket = buckets[key]
-                                if (bucket === doc.name) {
-                                    found = true
-                                    break
-                                }
-                            }
-                            if (!found) {
-                                Bucket.deleteOne({ _user: _user, name: doc.name }, (err, docs) => {
-                                    if (!err) {
-                                        console.log("Deleted Bucket")
-                                    } else {
-                                        res.json(err)
-                                    }
-                                })
-                            }
-                        })
-                    })
-                    var rank = 1
-                    for (var key in buckets) {
-                        // const bucket = { rank: rank, name: buckets[key].name, id: buckets[key].id }
-                        const bucket = { rank: rank, name: buckets[key] }
 
-                        console.log(buckets)
-                        rank += 1
-                        Bucket.findOneAndUpdate({ _user: _user, name: bucket.name }, bucket, { upsert: true }, (err, docs) => {
-                            if (!err) {
-                                console.log(docs)
-                            } else {
-                                res.json(err)
+                Bucket.find({ _user: _user }, (err, docs) => {
+                    docs.forEach(doc => {
+                        var found = false
+                        for (var key in buckets) {
+                            var bucket = buckets[key]
+                            if (bucket === doc.name) {
+                                found = true
+                                break
                             }
-                        })
-                    }
-                    res.json({ "message": "saved" })
-                } else {
-                    return res.status(401).json({
-                        success: 'false',
-                        message: 'Unauthorized access'
+                        }
+                        if (!found) {
+                            Bucket.deleteOne({ _user: _user, name: doc.name }, (err, docs) => {
+                                if (!err) {
+                                    console.log("Deleted Bucket")
+                                } else {
+                                    res.json(err)
+                                }
+                            })
+                        }
+                    })
+                })
+                var rank = 1
+                for (var key in buckets) {
+                    // const bucket = { rank: rank, name: buckets[key].name, id: buckets[key].id }
+                    const bucket = { rank: rank, name: buckets[key] }
+
+                    console.log(buckets)
+                    rank += 1
+                    Bucket.findOneAndUpdate({ _user: _user, name: bucket.name }, bucket, { upsert: true }, (err, docs) => {
+                        if (!err) {
+                            console.log(docs)
+                        } else {
+                            res.json(err)
+                        }
                     })
                 }
+                res.json({ "message": "saved" })
+
 
             }
         })
